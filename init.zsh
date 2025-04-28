6b5297601f8077d87b367041f1b6275bd62b4ef5
Jason L Connor <jason.connor@gmail.com>
2025-04-28 12:54:14 -0600

    Custom aliases

--- init.zsh
+++ init.zsh
@@ -1,4 +1,3 @@
-# This is example code.
-# This will be executed when this module is initialized during Zsh startup.
-
-print "executed code in ${0}"
+alias aider="op run --no-masking -- aider"
+alias k=kubectl
+alias kuse='kubectl config use-context'
