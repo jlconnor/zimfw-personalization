0481602fa3cfcca28bf43586b6b15ad34e6db6b3
Jason L Connor <jason.connor@gmail.com>
2025-04-28 22:11:35 -0600

    Editor env var

--- init.zsh
+++ init.zsh
@@ -1,15 +1,17 @@
+# Vim keybindings
 bindkey -v
 
+# Exports
+export EDITOR=vim
 export PAGER=""
+
+export PROTO_HOME="$HOME/.proto";
+export PATH=PATH="$PROTO_HOME/shims:$PROTO_HOME/bin:$HOME/.local/bin:$HOME/bin:$HOME/.ghcup/bin:$PATH"
 
 export ANTHROPIC_API_KEY=op://Personal/Anthropic/credential
 export OPENAI_API_KEY=op://Personal/OpenAI/credential
 export OPENROUTER_API_KEY=op://Personal/OpenRouter/credential
 export LITELLM_MASTER_KEY=op://Personal/LiteLLM/master-key
-
-export PROTO_HOME="$HOME/.proto";
-# Ensure that local bin directories are in the path and are searched first.
-export PATH=PATH="$PROTO_HOME/shims:$PROTO_HOME/bin:$HOME/.local/bin:$HOME/bin:$HOME/.ghcup/bin:$PATH"
 
 # Aliases
 alias aider="op run --no-masking -- aider"
