75575884bcc131abb9b11c25b59d224c33fac9cd
Jason L Connor <jason.connor@gmail.com>
2025-04-28 22:04:03 -0600

    Remaining customizations from existing oh-my-zsh custom plugins

--- init.zsh
+++ init.zsh
@@ -7,12 +7,12 @@ export OPENAI_API_KEY=op://Personal/OpenAI/credential
 export OPENROUTER_API_KEY=op://Personal/OpenRouter/credential
 export LITELLM_MASTER_KEY=op://Personal/LiteLLM/master-key
 
-alias aider="op run --no-masking -- aider"
+export PROTO_HOME="$HOME/.proto";
+# Ensure that local bin directories are in the path and are searched first.
+export PATH=PATH="$PROTO_HOME/shims:$PROTO_HOME/bin:$HOME/.local/bin:$HOME/bin:$HOME/.ghcup/bin:$PATH"
 
+# Aliases
+alias aider="op run --no-masking -- aider"
+alias hp=http-prompt
 alias k=kubectl
 alias kuse='kubectl config use-context'
-
-export PROTO_HOME="$HOME/.proto";
-
-# Ensure that local bin directories are in the path and are searched first.
-export PATH=PATH="$PROTO_HOME/shims:$PROTO_HOME/bin:$HOME/.local/bin:$HOME/bin:$PATH"
