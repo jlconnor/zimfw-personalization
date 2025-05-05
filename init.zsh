e673cee287b4f251c07b9a0a6c46b1fbf2752132
Jason L Connor <jason@takeup.ai>
2025-05-05 15:30:25 -0600

    Don't add the venv to the prompt

--- init.zsh
+++ init.zsh
@@ -13,6 +13,7 @@ export HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND=none
 export HOMEBREW_NO_ENV_HINTS=1
 export PAGER=""
 export PROTO_HOME="$HOME/.proto";
+export VIRTUAL_ENV_DISABLE_PROMPT=1
 
 # Path manipulations
 if [ -d "/Applications/SnowSQL.app/Contents/MacOS" ]; then
