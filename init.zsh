7820acba69bb792f7cea155166b1fd9c5efd36d0
Jason L Connor <jason.connor@gmail.com>
2025-06-01 02:14:38 -0600

    Fix openai key; remove vimlm

--- init.zsh
+++ init.zsh
@@ -24,7 +24,7 @@ export PATH="$PROTO_HOME/shims:$PROTO_HOME/bin:$HOME/.local/bin:$HOME/bin:$HOME/
 
 # Secrets in 1password
 export ANTHROPIC_API_KEY=op://Personal/Anthropic/credential
-export OPENAI_API_KEY=:op://Personal/OpenAI/credential
+export OPENAI_API_KEY=op://Personal/OpenAI/credential
 export SNOWSQL_ACCOUNT=op://Takeup.ai/Snowflake/accountname
 export SNOWSQL_USER=op://Takeup.ai/Snowflake/username
 export SNOWSQL_PWD=op://Takeup.ai/Snowflake/password
@@ -39,5 +39,4 @@ alias k=kubectl
 alias kuse="kubectl config use-context"
 alias paws="aws --profile prod"
 alias snowsql="op run --no-masking -- snowsql"
-alias vimlm="op run --no-masking -- vimlm"
 alias zed="op run --no-masking -- zed"
